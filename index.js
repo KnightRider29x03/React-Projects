388c01743c6214108e52a20d993feff4d74900e5
KnightRider29x03 <133299216+KnightRider29x03@users.noreply.github.com>
2024-01-17 18:50:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import Main from './Main';
-//import Contact from './Contact';
 import ToDoList from './ToDoList';
-//import FileSystem from "./FileSystem";
-//import Userform from "./UserProfile";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
